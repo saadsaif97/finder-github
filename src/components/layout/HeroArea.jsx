dd497f7bab1e87b6ec6290eaeffc4217ae9651ab
Nouman <goodblessnoman@gmail.com>
2021-06-20 15:26:42 +0500

    updated the logo on small screens

--- src/components/layout/HeroArea.jsx
+++ src/components/layout/HeroArea.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const HeroArea = () => {
   return (
     <div className='heroArea'>
+      <h1 className='text-center display-4'><i className='fab fa-github d-md-none'></i></h1>
       <h1 className='display-4 text-center font-weight-bolder'>
-        <i className='fab fa-github'></i> Github Finder
+        <i className='fab fa-github d-none d-md-inline'></i> Github Finder
       </h1>
       <p className='text-center'>search user and see the repositories</p>
     </div>
